ec88263f14681a62dc75056678976aa4558c4a52
Brad Alexander <brad.alexander913@gmail.com>
2019-08-30 01:52:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,13 @@ class Song
     self.all.find{|song| song.name == name} 
   end
   
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) 
+      self.find_by_name(name) 
+    else 
+      self.create_by_name(name)
+    end 
+  end
+      
+  
 end
