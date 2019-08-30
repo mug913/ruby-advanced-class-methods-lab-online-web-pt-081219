2d5386118c207c1cbd3986fc5eef595c127c90a7
Brad Alexander <brad.alexander913@gmail.com>
2019-08-30 01:02:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -5,9 +7,28 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    song = self.new 
+    @@all << song
+    song
+  end
 
   def save
     self.class.all << self
   end
-
-end
+  
+  
+  def name 
+    @self.name 
+  end
+ 
+ 
+  def self.new_by_name(name)
+    song = self.new
+    @@all << song
+    song.name = name
+  end 
+ 
+    
+  end
